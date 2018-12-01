8b7f9fd0fdafab36fb82f4096c1745f8e127eae0
Khalid <timoncr9@gmail.com>
2018-12-01 19:09:15 +0200

    [Authors microservice] Set the AuthorController structure.

---
+++ AuthorsApi/app/Http/Controllers/AuthorController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class AuthorController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function index()
+    {
+
+    }
+
+    public function store(Request $request)
+    {
+
+    }
+
+    public function show($author)
+    {
+
+    }
+
+    public function update(Request $request , $author)
+    {
+
+    }
+
+    public function destroy($author)
+    {
+
+    }
+}
