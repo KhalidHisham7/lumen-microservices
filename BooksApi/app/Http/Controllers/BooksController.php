bf0c1f306b60764861093e7fbb70ab8faf6377f0
Khalid <timoncr9@gmail.com>
2018-12-04 15:05:03 +0200

    [Books microservice] Created the BooksController and added the main structure of the functions.

---
+++ BooksApi/app/Http/Controllers/BooksController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BooksController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function index()
+    {
+
+    }
+
+    public function store(Request $request)
+    {
+
+    }
+
+    public function show($book)
+    {
+
+    }
+
+    public function update(Request $request , $book)
+    {
+
+    }
+
+    public function destroy($book)
+    {
+
+    }
+}
