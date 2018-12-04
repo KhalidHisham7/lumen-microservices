89c0dccaa9ee4335f493dda15c4b3380806092e0
Khalid <timoncr9@gmail.com>
2018-12-04 23:04:41 +0200

    [ApiGateway microservice] Added the main structure of the Controllers(Books, Authors) in the ApiGateway.

---
+++ ApiGateway/app/Http/Controllers/AuthorController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Traits\ApiResponser;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use App\Author;
+
+class AuthorController extends Controller
+{
+    use ApiResponser;
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function index()
+    {
+      
+    }
+
+    public function store(Request $request)
+    {
+
+    }
+
+    public function show($author)
+    {
+
+    }
+
+    public function update(Request $request , $author)
+    {
+
+    }
+
+    public function destroy($author)
+    {
+
+    }
+}
