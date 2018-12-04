b00a84f7a00be63cd29a0ffda8917702b8365b7f
Khalid <timoncr9@gmail.com>
2018-12-04 17:07:25 +0200

    [Books microservice] Created the update method in the BooksController.

--- BooksApi/app/Http/Controllers/BooksController.php
+++ BooksApi/app/Http/Controllers/BooksController.php
@@ -53,7 +53,25 @@ class BooksController extends Controller
 
     public function update(Request $request , $book)
     {
+      $rules = [
+        'title' => 'required|max:255',
+        'description' => 'required|max:255',
+        'price' => 'required|min:1|gte:1',
+        'author_id' => 'required|min:1',
+      ];
+      $this->validate($request, $rules);
 
+      $book = Book::findOrFail($book);
+      $book->fill($request->all());
+
+      if($book->isClean())
+      {
+        return $this->errorResponse("At least one value must change", Response::HTTP_UNPROCESSABLE_ENTITY);
+      }
+
+      $book->save();
+
+      return $this->succesResponse($book);
     }
 
     public function destroy($book)
