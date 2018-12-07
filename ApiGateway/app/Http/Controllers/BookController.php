a5d10a0a3d21d20f6abfe946bcde61c6657ebe23
Khalid <timoncr9@gmail.com>
2018-12-07 21:53:22 +0200

    [ApiGateway microservice] Added a check step to see if the author_id actually exists.

--- ApiGateway/app/Http/Controllers/BookController.php
+++ ApiGateway/app/Http/Controllers/BookController.php
@@ -6,6 +6,7 @@ use App\Traits\ApiResponser;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use App\Services\BookService;
+use App\Services\AuthorService;
 use App\Book;
 
 class BookController extends Controller
@@ -14,15 +15,17 @@ class BookController extends Controller
     use ApiResponser;
 
     public $bookService;
+    public $authorService;
 
     /**
      * Create a new controller instance.
      *
      * @return void
      */
-    public function __construct(BookService $bookService)
+    public function __construct(BookService $bookService, AuthorService $authorService)
     {
       $this->bookService = $bookService;
+      $this->authorService = $authorService;
     }
 
     public function index()
@@ -32,6 +35,7 @@ class BookController extends Controller
 
     public function store(Request $request)
     {
+      $this->authorService->obtainAuthor($request->author_id);
       return $this->succesResponse($this->bookService->createBook($request->all(), Response::HTTP_CREATED));
     }
 
