22000341dbe19a420e6ef44c01ca4b672b08b758
Khalid <timoncr9@gmail.com>
2018-12-07 18:42:44 +0200

    [ApiGateway microservice] Created the create method for the authors through the gateway.

--- ApiGateway/app/Http/Controllers/AuthorController.php
+++ ApiGateway/app/Http/Controllers/AuthorController.php
@@ -31,7 +31,7 @@ class AuthorController extends Controller
 
     public function store(Request $request)
     {
-
+      return $this->succesResponse($this->authorService->createAuthor($request->all(), Response::HTTP_CREATED));
     }
 
     public function show($author)
